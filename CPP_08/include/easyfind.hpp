b7b4aea3787e8f73df7ce7199944e29300b4fb19
Emanuela <emanuela.licameli@gmail.com>
2023-12-27 18:00:17 +0100

    Add files via upload

--- CPP_08/include/easyfind.hpp
+++ CPP_08/include/easyfind.hpp
@@ -6,21 +6,38 @@
 /*   By: emlicame <emlicame@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/12/20 18:36:46 by emlicame      #+#    #+#                 */
-/*   Updated: 2023/12/20 18:39:14 by emlicame      ########   odam.nl         */
+/*   Updated: 2023/12/27 17:49:24 by emanuela      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
-Write a function template easyfind that accepts a type T. It takes two parameters.
-The first one has type T and the second one is an integer.
-
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-tmeplate <typename T>
-typename T easyfind(T &container, int n){
-	
+#include <algorithm>    // std::find
+#include <vector>       // std::vector
+#include <list>       	// std::vector
+#include <exception>
+
+class NotFound : public std::exception
+{
+	public: 
+			virtual const char* what() const throw() {
+				return ("No occurrence is found");
+			}
+};
+			
+template <typename T>
+typename T::iterator easyfind(T &container, int n){
+
+	auto it = find (container.begin(), container.end(), n);
+  	if (it != container.end())
+   		return it; // Return iterator if value is found
+  	else
+    	throw NotFound();
 }
 
 
 #endif
+
+//calculate the index 
+//size_t index = std::distance(container.begin(), it);
